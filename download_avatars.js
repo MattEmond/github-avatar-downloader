a1301e6289551492442fc7c1ada761fbf0f20ac4
MattEmond <memond36@gmail.com>
2017-11-07 01:06:01 +0000

    Added final notes

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,7 @@ console.log(`Welcome to GitHub Avatar Downloader`);
 
 // Implement function
 function getRepoContributors(repoOwner, repoName, callback) {
-  // Add User-Agent header in the request
+  // Add User-Agent header in the request.  Gives the URL of where to look + my GitHub name and token (which is in a different file).
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -14,7 +14,7 @@ function getRepoContributors(repoOwner, repoName, callback) {
       'Authorization': secrets.GITHUB_TOKEN
     }
   };
-
+  //  Request function to check if there is any info being bassed to the callbacl.  If there is, we need to parse the body being returned.
   request(options, function(error, response, body) {
     if (callback) {
         callback(error, JSON.parse(body));
@@ -24,18 +24,21 @@ function getRepoContributors(repoOwner, repoName, callback) {
 
 
 function downloadImageByURL(url, filePath) {
+  // Request to pull the source URL.
   request.get(url)
   .on('error', function(err) {
     console.log("We have a problem");
     throw err;
   })
-
+  //  Write images to new folder
   .pipe(fs.createWriteStream("./Avatars/"+filePath+".jpg"));
 }
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+
+//  Callback function  used to provide info on what is being downloaded.
 function repoCallback(err, result) {
   if (err) {
     console.log(`We have an error`);
